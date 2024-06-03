bedbb49b812476bf94c737aed1385fac9d802498
Adam Ohlson <adam.ohlson0531@gmail.com>
2024-06-03 12:17:56 +0200

    Simple Calculator added

--- Program.cs
+++ Program.cs
@@ -11,4 +11,27 @@ for (int i = 1; i < 1000; i++)
 }
 
 Console.WriteLine(sum);
-/*
+*/
+
+// Simple Calculator
+
+/* using System.Formats.Asn1;
+
+Console.WriteLine("Choose any number:\n");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Choose another number:\n");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+double sum = num1 + num2;
+double diff = num1 - num2;
+double product = num1 * num2;
+double div = num1 / num2; 
+double pow = Math.Pow(num1, num2);
+
+Console.WriteLine($"Sum: {sum:F2}");
+Console.WriteLine($"Difference: {diff:F2}");
+Console.WriteLine($"Product: {product:F2}");
+Console.WriteLine($"Divsion: {div:F2}");
+Console.WriteLine($"Power to: {pow:F2}");
+*/
